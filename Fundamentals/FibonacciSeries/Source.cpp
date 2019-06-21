a6e622c715bd1c669b78a691ee306149c7e09415
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-06-21 20:02:06 +0530

    Constexpr demo

---
+++ Fundamentals/FibonacciSeries/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+constexpr int Fibonacci(int n) 
+{
+	switch (n)
+	{
+	case 0:
+		return 0;
+	case	1:
+		return 1;
+	default:
+		return Fibonacci(n - 1) + Fibonacci(n - 2);
+	}
+}
+
+int main()
+{
+	static_assert(Fibonacci(10) == 55, "Fibonacci of (10) must be 55!");
+	return 0;
+}
